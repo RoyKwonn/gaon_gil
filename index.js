eec08bea92c40faaae5b3d4c538c9a42a90c68ba
Seokhwan-Kwon <sychar05@gmail.com>
2021-11-05 08:05:51 +0000

    Fri, Nov 5

--- index.js
+++ index.js
@@ -29,9 +29,18 @@ app.get('/write', (req, res) => {
 });
 
 
-////******줌 테스트 페이지*******////
+
+////****** 테스트 페이지*******////
 app.get('/test', (req, res) => {
     res.sendFile(__dirname + '/test.html');
+});
+
+app.get('/test2', (req, res) => {
+    res.sendFile(__dirname + '/test2.html');
+});
+
+app.get('/box', (req, res) => {
+    res.sendFile(__dirname + '/box.html');
 });
 
 
@@ -45,32 +54,71 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-
+// 좌표 데이터를 바탕으로 전국 '도' 범죄데이터를 받아온다.
 io.on('connection', (socket) => {
     socket.on('send', (msg) => {
         var location = msg.split(',');
         connection.query('SELECT 살인, 강도, 성범죄, 절도, 폭력 FROM Crime.crime_data where ( X = \"' + location[0] + '\" and Y = \"' + location[1] + '\")', (error, rows, fields) => {
             if (error) throw error;
-            io.emit('response', rows);
+            io.to(socket.id).emit('response', rows);
         });
     });
 });
 
 
+// 좌표 데이터를 바탕으로'구'의 범죄데이터를 받아온다.
 io.on('connection', (socket) => {
     socket.on('send_seoul', (msg) => {
         var location = msg.split(',');
         connection.query('SELECT 살인, 강도, 성범죄, 절도, 폭력 FROM Crime.seoul_data where ( X = \"' + location[0] + '\" and Y = \"' + location[1] + '\")', (error, rows, fields) => {
             if (error) throw error;
-            io.emit('response', rows);
+            io.to(socket.id).emit('response', rows);
         });
     });
 });
 
 
+
+// '도' 지역명, 좌표 데이터를 받아온다.
 io.on('connection', (socket) => {
-    socket.on('msg_0', (msg) => {
-        console.log('------------');
+    socket.on('request_Nationwide_data', (msg) => {
+        connection.query('SELECT 지역, X, Y FROM Crime.crime_data;', (error, rows, fields) => {
+            if (error) throw error;
+            io.to(socket.id).emit('Nationwide_data', rows);
+        });
+    });
+});
+
+
+// 서울의 '구' 지역명, 좌표 데이터를 받아온다.
+io.on('connection', (socket) => {
+    socket.on('request_Seoul_data', (msg) => {
+        connection.query('SELECT 지역, X, Y FROM Crime.seoul_data; ', (error, rows, fields) => {
+            if (error) throw error;
+            io.to(socket.id).emit('Seoul_data', rows);
+        });
+    });
+});
+
+
+
+// 뉴스 데이터를 받아온다.
+io.on('connection', (socket) => {
+    socket.on('request_news', (msg) => {
+        connection.query('select url, title, body, reporting_date,img from Crime.News_data order by reporting_date desc limit 8;', (error, rows, fields) => {
+            if (error) throw error;
+            io.to(socket.id).emit('news_data', rows);
+        });
+    });
+});
+
+
+
+
+
+/***********테스트 콘솔 출력***********/
+io.on('connection', (socket) => {
+    socket.on('msg_00', (msg) => {
         console.log(msg);
     });
 });
